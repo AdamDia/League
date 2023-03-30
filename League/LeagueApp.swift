50b4a93126e7352f6be1af8ff0fb614780de9664
Adam EM <adam.diavolo@gmail.com>
2023-03-30 06:42:09 +0200

    Initial Commit

---
+++ League/LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueApp.swift
+//  League
+//
+//  Created by Adam Essam on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
